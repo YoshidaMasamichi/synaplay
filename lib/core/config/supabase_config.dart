4adb0c2de673ffd8cf68092153f69df9c69b83de
YoshidaMasamichi <uniters123@icloud.com>
2026-06-21 22:23:24 +0900

    Fix Supabase project URL

--- lib/core/config/supabase_config.dart
+++ lib/core/config/supabase_config.dart
@@ -1,4 +1,4 @@
 class SupabaseConfig {
-  static const url = 'https://taqewddlsqjjqfuvnthh.supabase.co';
+  static const url = 'https://taqewddlsqijqfuvnthh.supabase.co';
   static const anonKey = 'sb_publishable_Jbr1SPsb_DN44NgWPqErvQ_30Dczqbw';
 }
